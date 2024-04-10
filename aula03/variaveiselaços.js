781786ae3ec1cbf3d6a9bb646169ed420a2d4a76
Daniel Gondim <daniel.santos698@etec.sp.gov.br>
2024-04-10 08:33:33 -0300

    aula03 sobre arrays e objeto

---
+++ aula03/variaveiselaços.js
@@ -0,0 +1,24 @@
+let n = 2
+let t = "oi"
+
+console.log(typeof n)
+console.log(typeof t)
+
+let a = [0, 1, 1, 2, 3, 5, 8, 13, 21]
+
+console.log(typeof a)
+
+let o = {nome: "Daniel", idade: "36"} // JSON
+//JavaScript Object Nation
+console.log(typeof o)
+
+console.log(a)
+console.log(a[7])
+
+a.forEach ( (el) => 
+    console.log(`elemento: ${el}`)
+)
+
+console.log(o)
+console.log(o.nome, "tem", o.idade)
+
